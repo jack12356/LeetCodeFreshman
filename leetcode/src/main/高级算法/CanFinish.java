bd3896e3599e4ed21dfd92fe3591ec0b8567a41e
Nicco <18215568402@163.com>
2019-01-14 10:41:03 +0800

    -

--- leetcode/src/main/高级算法/CanFinish.java
+++ leetcode/src/main/高级算法/CanFinish.java
@@ -1,18 +1,16 @@
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class CanFinish {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[][] matrix = new int[numCourses][numCourses]; // i -> j //邻接矩阵存储图
+        int[][] Matrix = new int[numCourses][numCourses]; // i -> j //邻接矩阵存储图
         int[] indegree = new int[numCourses]; // 统计每个节点的入度
 
-        for (int i = 0; i < prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
-            int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0)
+        for (int[] prerequisite : prerequisites) {
+            int ready = prerequisite[0];
+            int pre = prerequisite[1];
+            if (Matrix[pre][ready] == 0)
                 indegree[ready]++;
-            matrix[pre][ready] = 1;
+            Matrix[pre][ready] = 1;
         }
 
         int count = 0;
@@ -25,7 +23,7 @@ public class CanFinish {
             int course = queue.poll();
             count++;
             for (int i = 0; i < numCourses; i++) {
-                if (matrix[course][i] != 0) {// 节点 i 与该节点相连
+                if (Matrix[course][i] != 0) {// 节点 i 与该节点相连
                     indegree[i]--; // 与刚出队的节点相连的节点，入度减一
                     if (indegree[i] == 0) { // 如果为0，说明没有前驱，可以访问
                         queue.offer(i);
@@ -34,6 +32,40 @@ public class CanFinish {
             }
         }
         return count == numCourses; // 如果所有节点是否都访问了，如果是说明成功
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[][] Matrix = new int[numCourses][numCourses]; // i -> j //邻接矩阵存储图
+        int[] indegree = new int[numCourses]; // 统计每个节点的入度
+
+        for (int[] prerequisite : prerequisites) {
+            int ready = prerequisite[0];
+            int pre = prerequisite[1];
+            if (Matrix[pre][ready] == 0)
+                indegree[ready]++;
+            Matrix[pre][ready] = 1;
+        }
+
+        int count = 0;
+        Queue<Integer> queue = new LinkedList();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0)
+                queue.offer(i);
+        }
+        int[] res = new int[numCourses];
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            res[count++] = course;
+            for (int i = 0; i < numCourses; i++) {
+                if (Matrix[course][i] != 0) {// 节点 i 与该节点相连
+                    indegree[i]--; // 与刚出队的节点相连的节点，入度减一
+                    if (indegree[i] == 0) { // 如果为0，说明没有前驱，可以访问
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+        return count == numCourses ? res : new int[numCourses]; // 如果所有节点是否都访问了，如果是说明成功
     }
 
     public static void main(String[] args) {
